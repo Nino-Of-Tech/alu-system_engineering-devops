701c0fb51de47722578de189b58d8071080266a6
Nino-Of-Tech <nwachukwunino15@gmail.com>
2022-12-09 01:29:36 +0000

    new

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 #check the error and duly fix the bug
-
-exec { 'delete a file':
-    command => 'sed -i s/class-wp-locale.phpp/class-wp-locale.php/g /var/www/html/wp-settings.php',
-    provider => shell
+exec { 'change':
+    command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+    provider => shell,
 }
